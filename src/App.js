aaa6ebe5627267a9a5afa5de1eee3532ddea4b29
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-16 21:07:15 +0530

    Added Header & Figure Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
+import Header from './components/Header'
+import Figure from './components/Figure'
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
+
+    </>
   );
 }
 
